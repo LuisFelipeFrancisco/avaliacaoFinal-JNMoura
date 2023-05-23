a87d426485ef6c9ca17f727ebf903e960112aeca
Luis Felipe Francisco <luis.ferreira36@fatec.sp.gov.br>
2023-05-23 14:25:31 -0300

    feat: services, menu, error components

--- Angular/web-angular/src/app/app.module.ts
+++ Angular/web-angular/src/app/app.module.ts
@@ -4,9 +4,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
+import { MensagemErro404Component } from './components/mensagem/mensagem-erro404/mensagem-erro404.component';
+import { MenuComponent } from './components/menu/menu.component';
 import { VeiculoCreateComponent } from './components/veiculo/veiculo-create/veiculo-create.component';
 import { VeiculoEditComponent } from './components/veiculo/veiculo-edit/veiculo-edit.component';
 import { VeiculoIndexComponent } from './components/veiculo/veiculo-index/veiculo-index.component';
+
 
 @NgModule({
   declarations: [
@@ -14,7 +17,9 @@ import { VeiculoIndexComponent } from './components/veiculo/veiculo-index/veicul
     HomeComponent,
     VeiculoCreateComponent,
     VeiculoEditComponent,
-    VeiculoIndexComponent
+    VeiculoIndexComponent,
+    MensagemErro404Component,
+    MenuComponent
   ],
   imports: [
     BrowserModule,
---
+++ Angular/web-angular/src/app/components/mensagem/mensagem-erro404/mensagem-erro404.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-mensagem-erro404',
+  templateUrl: './mensagem-erro404.component.html',
+  styleUrls: ['./mensagem-erro404.component.css']
+})
+export class MensagemErro404Component {
+
+}
